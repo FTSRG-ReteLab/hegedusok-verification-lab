b91d87308d2a4da1e56ecb8c9be600f803b3727d
hegedusfanni <fanni.hegedus95@gmail.com>
2017-03-13 07:39:42 -0700

    periodikus follow  2 fanni

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,30 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	Timer timer;
+	TimerTask task;
+
+	TrainControllerImpl(){
+		timer=new Timer();
+		task= new TimerTask(){
+			@Override
+			public void run(){
+				followSpeed();
+			}
+		};
+
+		timer.schedule(task, 0, 1000);
+	}
+
 
 	@Override
 	public void followSpeed() {
